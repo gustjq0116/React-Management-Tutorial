251ef683f445b7ce2fc5c4f08b605538092f11d5
gustjq0116 <a01065067152@gmail.com>
2020-08-17 08:09:39 +0900

    Create customer database table

--- managerment/server.js
+++ managerment/server.js
@@ -1,39 +1,36 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var port = process.env.PORT || 5000;
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+})
+
+conn.connect();
+
 app.get('/api/customers', (req, res) => 
 {
-    res.send([
+    conn.query(
+        "SELECT * FROM customer",
+        (err, rows, fields) => 
         {
-            'id':1,
-            'image':'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birthday': '921222',
-            'gender': '남자',
-            'job': '대학생'
-          },
-          {
-            'id':2,
-            'image':'https://placeimg.com/64/64/2',
-            'name': '송병호',
-            'birthday': '921121',
-            'gender': '남자',
-            'job': '디자이너'
-          },
-          {
-            'id':3,
-            'image':'https://placeimg.com/64/64/3',
-            'name': '전기범',
-            'birthday': '920312',
-            'gender': '남자',
-            'job': '사람'
-          }
-    ]);
+            res.send(rows);
+        }
+    )
+    
 })
 
 app.listen(port, () => console.log('Listening on port'));
