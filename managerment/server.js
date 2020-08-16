ad02788e94731c0a102d64dffb3a516d8a8e09c8
gustjq0116 <a01065067152@gmail.com>
2020-08-16 14:16:01 +0900

    Add Node.js express server module

---
+++ managerment/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/api/hello', (req, res) =>
+{
+    res.send({message: 'Hello Express! '});
+
+});
+
+app.listen(port, () => console.log('Listening on port'));
